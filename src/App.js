f3b2f70e08ad1852784eca4f43505a5354918d7d
Loren Pabst <lorenpabst@Lorens-MacBook-Pro.local>
2020-11-27 21:24:32 -0700

    added styles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <p>this is for holding text</p>
-      <input placeholder='email' />
-      <input type='checkbox' />
-      <input type="radio" />
-      <button>click me</button>
-      <img 
-        src='https://image.shutterstock.com/image-photo/american-staffordshire-terrier-puppies-sitting-260nw-1048123303.jpg' 
-        alt='puppies' 
-      />
+      <div className='header'>
+        <div className='nav'>
+          <p className='whiteText'>Cars for Sales</p>
+          <p className='whiteText'>Sell Your Car</p>
+          <p className='whiteText'>Service & Repair</p>
+          <p className='whiteText'>Research</p>
+          <p className='whiteText'>Videos & Reviews</p>
+          <p className='whiteText'>Contact Us</p>
+        </div>
+      </div>
 
+      <div className='pinkBar'></div>
 
+      <div className='jumboImageContainer'>
+        <img 
+          src="https://www.cars.com/cldstatic/ads/creative/20201118_5428_MY21_Elantra_Hero_2400x753.jpg" 
+          alt='car' 
+          className='jumboImage' 
+        />
+        <p className='jumboImageText'>Find your next match</p>
+        <div className='sponsorshipBox'>
+          <p className='whiteText'>Sponsored by</p>
+          <img src='' alt='hyundai logo' />
+          <p className='whiteText'>2021 Hyundai ELANTRA</p>
+        </div>
+      </div>
 
+      <div className='borderedBox'>
+
+      </div>
     </div>
   );
 }
